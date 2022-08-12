d3f63de2c29af9d7a4fb6abbd412c329b91ab012
nabse <nnabse@gmail.com>
2022-08-12 15:07:54 +0300

    auth pages, routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignInComponent } from '@pages/auth/sign-in/sign-in.component';
+import { SignUpComponent } from '@pages/auth/sign-up/sign-up.component';
+
+const routes: Routes = [
+  { path: 'signUp', component: SignUpComponent },
+  { path: 'signIn', component: SignInComponent },
+  { path: '**', component: SignUpComponent },
+  { path: '', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
