8e91fddd4b7e21da26379e93dfb2af6691c4cc1e
nnabse <nnabse@gmail.com>
2022-08-23 13:36:31 +0300

    add body type in http auth helper

--- src/app/services/http-auth-helper.service.ts
+++ src/app/services/http-auth-helper.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { DB_LINK } from '@constants/db-links.constants';
+import { User } from '@interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +11,11 @@ import { DB_LINK } from '@constants/db-links.constants';
 export class HttpAuthHelperService {
   constructor(private http: HttpClient) {}
 
-  public loginUser<T>(link: string, body: object): Observable<T> {
+  public loginUser<T>(link: string, body: User): Observable<T> {
     return this.http.post<T>(`${DB_LINK}${link}`, body);
   }
 
-  public registerUser<T>(link: string, body: any): Observable<T> {
+  public registerUser<T>(link: string, body: User): Observable<T> {
     return this.http.post<T>(`${DB_LINK}${link}`, body);
   }
 }
